65614d4da76594c7965747574d132c277687e5e6
dani0001414 <dani0001414@gmail.com>
2019-04-05 19:38:07 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ function HttpGet(url) {
 
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
-        document.getElementsByClassName("content")[0].innerHTML = "Töltődik...";
+        document.getElementsByClassName("content")[0].innerHTML = "<div class=\"logotext\"><center>Épp kurblizzuk az API-ból...</center></div>";
         if (this.readyState == 4 && this.status == 200) {
             // callback(xhttp.responseText);
             document.getElementsByClassName("content")[0].innerHTML = xhttp.responseText;
