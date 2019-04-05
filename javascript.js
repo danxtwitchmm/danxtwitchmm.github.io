f5e40dfe692f3583266cf392451e56335d455c48
dani0001414 <dani0001414@gmail.com>
2019-04-05 19:32:52 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,10 +1,17 @@
 function HttpGet(url) {
-    window.addEventListener('load', function(){
+
+    
+   
+    window.onload = function ()  {
         document.getElementsByClassName("content")[0].innerHTML = "<center>Töltődik...</center>";
-      });
+      }
+
+      
+    
+     
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
-        
+
         if (this.readyState == 4 && this.status == 200) {
             // callback(xhttp.responseText);
             document.getElementsByClassName("content")[0].innerHTML = xhttp.responseText;
@@ -12,6 +19,7 @@ function HttpGet(url) {
     };
     xhttp.open("GET", url, true);
     xhttp.send();
+
 
 }
 
